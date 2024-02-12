8ddc91e92849a766b3bf09a358531d5f4722c8bc
Zaher Zeinni <zaherzeinni@hotmail.com>
2024-02-13 00:57:25 +0200

    okkk

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
 import '@/styles/globals.css'
-import { Provider } from 'react-redux'
-import store from '@/redux/store'
-
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 export default function App({ Component, pageProps }) {
   
   return (
   <div>
-    <Provider store={store}>
+    <ChakraProvider>
   <Component {...pageProps} />
-  </Provider>
+  </ChakraProvider>
   </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,26 @@
-import { useEffect, useState } from "react";
 
-import { selectAllUsers } from "../redux/userSlice";
-import { selectAllPosts } from "../redux/postSlice";
-import { useSelector } from "react-redux";
-import TestForm from '../components/testForm'
+function Home({res}) {
 
-//import PostForm from "../components/postForm";
-//import PostList from "../components/postList";
-
-function Home() {
-  const [count, setCount] = useState(0);
-
-  const users = useSelector(selectAllUsers);
-  const posts = useSelector(selectAllPosts);
-
-  // get AllData from test branch
-  //const {name ,age} = useSelector((state)=>state.test)
-  const testData = useSelector((state) => state.test);
-  console.log("TESTDATA-->", testData);
-
-  useEffect(() => {}, []);
 
   return (
     <div>
-      <TestForm />
-      {/* {users?.length} */}
-
-      {/* <PostForm users={users} />
-
-      <PostList posts={posts}  /> */}
+    hiii
+       
     </div>
   );
 }
 
 export default Home;
+
+
+export const getServerSideProps = async()=>{
+
+
+  // serverSideRedirect(res, `${process.env.BASE_PATH}${products}`, 307);
+
+  // res.end();
+
+  // return { props: { baseUrl, submissionPredictions } };
+};
+
+
