5c1aa4e79922a07eb51ebf0d73a829d200a73441
Zaher Zeinni <zaherzeinni@gmail.com>
2024-02-20 00:08:55 +0200

    new

--- components/profiles.js
+++ components/profiles.js
@@ -1,5 +1,12 @@
 import React, { useState } from 'react';
 import { Data } from './data';
+import {
+    Select,
+    MenuItem,
+    FormControl,
+    InputLabel,
+    Container,
+  } from "@mui/material";
 
 
 
@@ -21,36 +28,43 @@ const handleOnChange=(e)=> {
     const id = Number(e.target.value)
     console.log("ID",id)
     
-    const subDetails = details.find((item)=>item.id===id)
-    console.log("subDetails",subDetails)
-    setActiveIndex(subDetails)
+
 }
 
 
     return (
         <div>
-       
- <div className='flex items-center  text-center justify-center'>
-         
-        <div >
+  <Container>
+  <FormControl
+        fullWidth
+        sx={{ marginTop: "40px", marginBottom: "40px" }}
+        variant="outlined"
+      >
+        <InputLabel> Category </InputLabel>
+        <Select label="categoryyy" onChange={handleOnChange}>
+          {Data.map((data) => (
+            <MenuItem key={data.id} value={data.id}>
+              {data.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
         
-        <h2>
-            {details.length}
-        </h2>
-
-        </div>
-        
-        <div className='flex  justify-center items-center !mx-2'>
+      </Container>
+      <div className='flex  justify-center items-center !mx-2'>
         {Data.map((item,index)=>(
             <img key={index} src={item.image} onClick={() => handleClick(index)} />
             
             ))}
         </div>
-        
         </div>
     
-   </div>
     );
 }
 
 export default Profiles;
+
+
+
+
+
